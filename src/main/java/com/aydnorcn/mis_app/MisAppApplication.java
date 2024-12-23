3e2f06399c0678b804d1573022c407d44921a0a8
aydnorcn <orcnaydn3535@gmail.com>
2024-12-23 22:16:09 +0300

    First Commit

---
+++ src/main/java/com/aydnorcn/mis_app/MisAppApplication.java
@@ -0,0 +1,12 @@
+package com.aydnorcn.mis_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MisAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MisAppApplication.class, args);
+    }
+}
