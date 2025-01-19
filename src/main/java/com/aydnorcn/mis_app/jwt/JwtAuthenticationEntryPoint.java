74f751fb8fca051d647ec9f198be269e3a6cede3
aydnorcn <orcnaydn3535@gmail.com>
2025-01-19 13:52:30 +0300

    fix: Update exception error message handler on JwtAuthenticationEntryPoint

--- src/main/java/com/aydnorcn/mis_app/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/aydnorcn/mis_app/jwt/JwtAuthenticationEntryPoint.java
@@ -1,19 +1,30 @@
 package com.aydnorcn.mis_app.jwt;
 
+import com.aydnorcn.mis_app.exception.ErrorMessage;
+import com.aydnorcn.mis_app.utils.MessageConstants;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Date;
 
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
-
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws RuntimeException, IOException {
+        ErrorMessage errorMessage = new ErrorMessage(new Date(), MessageConstants.AUTHENTICATION_REQUIRED);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        OutputStream responseStream = response.getOutputStream();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(responseStream, errorMessage);
+        responseStream.flush();
     }
 }
