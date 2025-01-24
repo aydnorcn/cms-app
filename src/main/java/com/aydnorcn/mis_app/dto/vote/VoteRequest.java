0f048989edb5d9dff1852c512e062956754ab890
aydnorcn <orcnaydn3535@gmail.com>
2025-01-24 13:57:14 +0300

    chore: Add setter for VoteRequest

--- src/main/java/com/aydnorcn/mis_app/dto/vote/VoteRequest.java
+++ src/main/java/com/aydnorcn/mis_app/dto/vote/VoteRequest.java
@@ -2,10 +2,12 @@ package com.aydnorcn.mis_app.dto.vote;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
+import lombok.Setter;
 
 import static com.aydnorcn.mis_app.utils.MessageConstants.*;
 
 @Getter
+@Setter
 public class VoteRequest {
 
     @NotBlank(message = OPTION_ID_NOT_BLANK)
