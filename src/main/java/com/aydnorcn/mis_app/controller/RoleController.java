51b1c86e8e0a31c5c036db0b4754566ecec78dbe
aydnorcn <orcnaydn3535@gmail.com>
2025-01-26 22:32:43 +0300

    feat: Add @Validated annotation to methods

--- src/main/java/com/aydnorcn/mis_app/controller/RoleController.java
+++ src/main/java/com/aydnorcn/mis_app/controller/RoleController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -30,13 +31,13 @@ public class RoleController {
 
     @PostMapping
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<Role> createRole(@RequestBody CreateRoleRequestDto role) {
+    public ResponseEntity<Role> createRole(@Validated @RequestBody CreateRoleRequestDto role) {
         return ResponseEntity.status(HttpStatus.CREATED).body(roleService.createRole(role));
     }
 
     @PutMapping("/{roleId}")
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<Role> updateRole(@PathVariable String roleId, @RequestBody CreateRoleRequestDto role) {
+    public ResponseEntity<Role> updateRole(@PathVariable String roleId, @Validated @RequestBody CreateRoleRequestDto role) {
         return ResponseEntity.ok(roleService.updateRole(roleId, role));
     }
 
