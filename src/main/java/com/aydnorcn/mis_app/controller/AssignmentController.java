20dae5c087548cc16ce76cb7e84c42c6e88ad14c
aydnorcn <orcnaydn3535@gmail.com>
2025-02-07 20:59:04 +0300

    docs: Add documentation to AssignmentController and RoleController

--- src/main/java/com/aydnorcn/mis_app/controller/AssignmentController.java
+++ src/main/java/com/aydnorcn/mis_app/controller/AssignmentController.java
@@ -5,8 +5,15 @@ import com.aydnorcn.mis_app.dto.assignment.AssignmentResponse;
 import com.aydnorcn.mis_app.dto.assignment.CreateAssignmentRequest;
 import com.aydnorcn.mis_app.dto.assignment.PatchAssignmentRequest;
 import com.aydnorcn.mis_app.entity.Assignment;
+import com.aydnorcn.mis_app.exception.ErrorMessage;
 import com.aydnorcn.mis_app.service.AssignmentService;
 import com.aydnorcn.mis_app.utils.params.AssignmentParams;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,15 +27,38 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/assignments")
 @RequiredArgsConstructor
+@Tag(name = "Assignment Controller")
 public class AssignmentController {
 
     private final AssignmentService assignmentService;
 
+    @Operation(
+            summary = "Retrieve assignment by id"
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Assignment found",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = AssignmentResponse.class))),
+                    @ApiResponse(responseCode = "404", description = "Assignment not found | If given id is not exists in database",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
+            }
+    )
     @GetMapping("/{assignmentId}")
     public ResponseEntity<AssignmentResponse> getAssignmentById(@PathVariable String assignmentId) {
         return ResponseEntity.ok(new AssignmentResponse(assignmentService.getAssignmentById(assignmentId)));
     }
 
+    @Operation(
+            summary = "Retrieve assignments by filtering and pagination"
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Assignments retrieved",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = PageResponseDto.class))),
+                    @ApiResponse(responseCode = "404", description = "Entity not found | If given (assignedTo, event, createdBy) params id is not exists in database",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+            }
+    )
     @GetMapping
     public ResponseEntity<PageResponseDto<AssignmentResponse>> getAssignments(@RequestParam Map<String, Object> searchParams) {
         AssignmentParams params = new AssignmentParams(searchParams);
@@ -41,24 +71,79 @@ public class AssignmentController {
         );
     }
 
+    @Operation(
+            summary = "Create a new assignment"
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "201", description = "Assignment created",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = AssignmentResponse.class))),
+                    @ApiResponse(responseCode = "400", description = "Bad request | If request value(s) are not valid",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "403", description = "Not have permission to access | If user not admin or moderator",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "404", description = "Entity not found | If given (assignedTo, event, createdBy) params id is not exists in database",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+            }
+    )
     @PostMapping
     @PreAuthorize("hasRole('ADMIN') or hasRole('MODERATOR')")
     public ResponseEntity<AssignmentResponse> createAssignment(@Validated @RequestBody CreateAssignmentRequest request) {
         return ResponseEntity.status(HttpStatus.CREATED).body(new AssignmentResponse(assignmentService.createAssignment(request)));
     }
 
+    @Operation(
+            summary = "Update all required parts of assignment"
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Assignment updated",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = AssignmentResponse.class))),
+                    @ApiResponse(responseCode = "400", description = "Bad request | If request value(s) are not valid",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "403", description = "Not have permission to access | If user not admin or moderator",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "404", description = "Entity not found | If given (assignedTo, event, createdBy) params id or assignment id is not exists in database",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
+            }
+    )
     @PutMapping("/{assignmentId}")
     @PreAuthorize("hasRole('ADMIN') or hasRole('MODERATOR')")
     public ResponseEntity<AssignmentResponse> updateAssignment(@PathVariable String assignmentId, @Validated @RequestBody CreateAssignmentRequest request) {
         return ResponseEntity.ok(new AssignmentResponse(assignmentService.updateAssignment(assignmentId, request)));
     }
 
+    @Operation(
+            summary = "Update partial parts of assignment"
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Assignment patched",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = AssignmentResponse.class))),
+                    @ApiResponse(responseCode = "403", description = "Not have permission to access | If user not admin or moderator",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "404", description = "Entity not found | If given (assignedTo, event, createdBy) params id or assignment id is not exists in database",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
+            }
+    )
     @PatchMapping("/{assignmentId}")
     @PreAuthorize("hasRole('ADMIN') or hasRole('MODERATOR')")
     public ResponseEntity<AssignmentResponse> patchAssignment(@PathVariable String assignmentId, @Validated @RequestBody PatchAssignmentRequest request) {
         return ResponseEntity.ok(new AssignmentResponse(assignmentService.patchAssignment(assignmentId, request)));
     }
 
+    @Operation(
+            summary = "Delete assignment by giving id"
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "204", description = "Assignment deleted"),
+                    @ApiResponse(responseCode = "403", description = "Not have permission to access | If user not admin or moderator",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "404", description = "Entity not found | If assignment id is not exists in database",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
+            }
+    )
     @DeleteMapping("/{assignmentId}")
     @PreAuthorize("hasRole('ADMIN') or hasRole('MODERATOR')")
     public ResponseEntity<Void> deleteAssignment(@PathVariable String assignmentId) {
--- src/main/java/com/aydnorcn/mis_app/controller/RoleController.java
+++ src/main/java/com/aydnorcn/mis_app/controller/RoleController.java
@@ -2,7 +2,14 @@ package com.aydnorcn.mis_app.controller;
 
 import com.aydnorcn.mis_app.dto.role.CreateRoleRequestDto;
 import com.aydnorcn.mis_app.entity.Role;
+import com.aydnorcn.mis_app.exception.ErrorMessage;
 import com.aydnorcn.mis_app.service.RoleService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,34 +20,107 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/roles")
 @RequiredArgsConstructor
+@Tag(name = "Role Controller")
 public class RoleController {
 
     private final RoleService roleService;
 
+    @Operation(
+            summary = "Retrieve role by id"
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Role found",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = Role.class))),
+                    @ApiResponse(responseCode = "404", description = "Role not found | If given id is not exists in database",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "403", description = "Not have permission to access | If user not admin or moderator",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
+            }
+    )
     @GetMapping("/{roleId}")
     @PreAuthorize("hasRole('ADMIN') or hasRole('MODERATOR')")
     public ResponseEntity<Role> getRoleById(@PathVariable String roleId) {
         return ResponseEntity.ok(roleService.getRoleById(roleId));
     }
 
+
+    @Operation(
+            summary = "Retrieve role by name"
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Role found",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = Role.class))),
+                    @ApiResponse(responseCode = "404", description = "Role not found | If given name is not exists in database",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "403", description = "Not have permission to access | If user not admin or moderator",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
+            }
+    )
     @GetMapping
     @PreAuthorize("hasRole('ADMIN') or hasRole('MODERATOR')")
     public ResponseEntity<Role> getRoleByName(@RequestParam String name) {
         return ResponseEntity.ok(roleService.getRoleByName(name));
     }
 
+
+    @Operation(
+            summary = "Create a new role"
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "201", description = "Role created",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = Role.class))),
+                    @ApiResponse(responseCode = "400", description = "Bad request | If role name is empty or not enough length",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "403", description = "Not have permission to access | If user not admin",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "409", description = "Conflict | If role name already exists",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
+            }
+    )
     @PostMapping
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Role> createRole(@Validated @RequestBody CreateRoleRequestDto role) {
         return ResponseEntity.status(HttpStatus.CREATED).body(roleService.createRole(role));
     }
 
+    @Operation(
+            summary = "Update role name by id"
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Role updated",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = Role.class))),
+                    @ApiResponse(responseCode = "400", description = "Bad request | If role name is empty or not enough length",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "403", description = "Not have permission to access | If user not admin",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "404", description = "Role not found | If given id is not exists in database",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "409", description = "Conflict | If role name already exists",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
+            }
+    )
     @PutMapping("/{roleId}")
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Role> updateRole(@PathVariable String roleId, @Validated @RequestBody CreateRoleRequestDto role) {
         return ResponseEntity.ok(roleService.updateRole(roleId, role));
     }
 
+    @Operation(
+            summary = "Delete role by giving id"
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "204", description = "Role deleted"),
+                    @ApiResponse(responseCode = "403", description = "Not have permission to access | If user not admin",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "404", description = "Role not found | If given id is not exists in database",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
+            }
+    )
     @DeleteMapping("/{roleId}")
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Void> deleteRole(@PathVariable String roleId) {
